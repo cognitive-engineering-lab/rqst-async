dc539ae7e5a5e8ca16baa1936b52376b07ad06ef
Will Crichton <wcrichto@cs.stanford.edu>
2024-07-12 13:39:18 -0700

    Solution for blocking chatbot

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use axum::{
     response::Html,
@@ -5,6 +7,7 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
+use tokio::join;
 
 async fn root() -> Html<&'static str> {
     Html(include_str!("../index.html"))
@@ -15,11 +18,15 @@ struct Chat {
     messages: Vec<String>,
 }
 
-async fn chat(Json(mut chat): Json<Chat>) -> Json<Chat> {
-    let mut responses = chatbot::query_chat(&chat.messages);
+async fn chat(Json(chat): Json<Chat>) -> Json<Chat> {
+    let chat_arc = Arc::new(chat);
+    let chat_arc_ref = Arc::clone(&chat_arc);
+    let responses_fut = tokio::spawn(async move { chatbot::query_chat(&chat_arc_ref.messages) });
     let random_fut = chatbot::gen_random_number();
-    let random = random_fut.await;
+    let (responses, random) = join!(responses_fut, random_fut);
+    let mut responses = responses.unwrap();
     let response = responses.remove(random % responses.len());
+    let mut chat = Arc::into_inner(chat_arc).unwrap();
     chat.messages.push(response);
     Json(chat)
 }
