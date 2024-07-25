7a139656ca3b39e0560c722a794f3028470cc2e2
Will Crichton <wcrichto@cs.stanford.edu>
2024-07-24 21:58:24 -0700

    Improve buffering

--- crates/miniserve/src/protocol.rs
+++ crates/miniserve/src/protocol.rs
@@ -3,8 +3,8 @@
 //! You should not need to deal with this module.
 
 use std::{
-    io::{self, BufRead, BufReader, Read, Write},
-    net::TcpStream,
+    io::{self, BufRead, BufReader, BufWriter, Read, Write},
+    net::{Shutdown, TcpStream},
 };
 
 use http::StatusCode;
@@ -12,85 +12,63 @@ use http::StatusCode;
 pub fn stringify_response(response: http::Response<Vec<u8>>) -> Vec<u8> {
     let (parts, body) = response.into_parts();
 
-    let mut response_buf = Vec::new();
-    write!(&mut response_buf, "HTTP/1.1 {}", parts.status.as_str()).unwrap();
+    let mut buf = Vec::with_capacity(body.len() + 256);
+    buf.extend_from_slice(b"HTTP/1.1 ");
+    buf.extend(parts.status.as_str().as_bytes());
     if let Some(reason) = parts.status.canonical_reason() {
-        write!(&mut response_buf, " {reason}").unwrap();
+        buf.extend_from_slice(b" ");
+        buf.extend(reason.as_bytes());
     }
-    write!(&mut response_buf, "\r\n").unwrap();
+
+    buf.extend_from_slice(b"\r\n");
 
     for (name, value) in parts.headers {
         if let Some(name) = name {
-            write!(&mut response_buf, "{}: ", name.as_str()).unwrap();
+            buf.extend(name.as_str().as_bytes());
+            buf.extend_from_slice(b": ");
         }
-        response_buf.extend(value.as_bytes());
-        write!(&mut response_buf, "\r\n").unwrap();
+        buf.extend(value.as_bytes());
+        buf.extend_from_slice(b"\r\n");
     }
 
-    write!(&mut response_buf, "\r\n").unwrap();
-    response_buf.extend(body);
+    buf.extend_from_slice(b"\r\n");
+    buf.extend(body);
 
-    response_buf
+    buf
 }
 
-fn read_request<'buf>(
-    stream: &mut TcpStream,
-    req_buf: &'buf mut Vec<u8>,
-    req: &mut httparse::Request<'_, 'buf>,
-    body_buf: &mut Vec<u8>,
-) -> Result<(), http::Response<Vec<u8>>> {
-    let reader = &mut BufReader::new(stream);
-    for line_res in reader.lines() {
-        match line_res {
-            Ok(line) => {
-                req_buf.extend(line.as_bytes());
-                req_buf.extend(b"\r\n");
-                if line.is_empty() {
-                    break;
-                }
-            }
-            Err(_) => {
-                return Err(make_response(
-                    StatusCode::BAD_REQUEST,
-                    "Failed to read stream",
-                ))
-            }
-        }
-    }
-
-    match req.parse(req_buf) {
-        Err(_) => {
-            return Err(make_response(
-                StatusCode::BAD_REQUEST,
-                "Failed to parse request",
-            ))
-        }
-        Ok(status) if status.is_partial() => {
-            return Err(make_response(StatusCode::BAD_REQUEST, "Partial request"));
-        }
-        _ => {}
-    }
-
-    let content_length = req
-        .headers
-        .iter()
-        .find(|header| header.name == "Content-Length");
-    if let Some(header) = content_length {
-        let Ok(length) = String::from_utf8_lossy(header.value).parse::<usize>() else {
-            return Err(make_response(
-                StatusCode::BAD_REQUEST,
-                "Invalid content length",
-            ));
-        };
-
-        body_buf.clear();
-        body_buf.extend((0..length).map(|_| 0));
-        if reader.read_exact(body_buf).is_err() {
-            return Err(make_response(StatusCode::BAD_REQUEST, "Invalid body"));
-        }
+fn parse_request(src: &[u8]) -> Result<Option<http::Request<Vec<u8>>>, http::Response<Vec<u8>>> {
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut parsed_req = httparse::Request::new(&mut headers);
+    let Ok(status) = parsed_req.parse(src) else {
+        return Err(make_response(
+            StatusCode::BAD_REQUEST,
+            "Failed to parse request",
+        ));
+    };
+    let amt = match status {
+        httparse::Status::Complete(amt) => amt,
+        httparse::Status::Partial => return Ok(None),
     };
 
-    Ok(())
+    let Ok(method) = http::Method::try_from(parsed_req.method.unwrap()) else {
+        return Err(make_response(
+            StatusCode::BAD_REQUEST,
+            "Failed to parse request",
+        ));
+    };
+
+    let data = &src[amt..];
+
+    let mut builder = http::Request::builder()
+        .method(method)
+        .version(http::Version::HTTP_11)
+        .uri(parsed_req.path.unwrap());
+    for header in parsed_req.headers {
+        builder = builder.header(header.name, header.value);
+    }
+
+    Ok(Some(builder.body(data.to_vec()).unwrap()))
 }
 
 fn make_response(status: http::StatusCode, explanation: &str) -> http::Response<Vec<u8>> {
@@ -101,29 +79,17 @@ fn make_response(status: http::StatusCode, explanation: &str) -> http::Response<
 }
 
 fn generate_response(
-    stream: &mut TcpStream,
+    req: http::Request<Vec<u8>>,
     callback: impl Fn(&str, crate::Request) -> Option<crate::Response>,
 ) -> http::Response<Vec<u8>> {
-    let mut headers = [httparse::EMPTY_HEADER; 64];
-    let mut req = httparse::Request::new(&mut headers);
-    let mut req_buf = Vec::new();
-    let mut body_buf = Vec::new();
-
-    if let Err(resp) = read_request(stream, &mut req_buf, &mut req, &mut body_buf) {
-        return resp;
-    }
-
-    let Some(path) = req.path else {
-        return make_response(StatusCode::BAD_REQUEST, "Missing path");
-    };
-
-    let request = match req.method {
-        Some("GET") => crate::Request::Get,
-        Some("POST") => crate::Request::Post(String::from_utf8(body_buf).unwrap()),
+    let (parts, body) = req.into_parts();
+    let request = match parts.method {
+        http::Method::GET => crate::Request::Get,
+        http::Method::POST => crate::Request::Post(String::from_utf8(body).unwrap()),
         _ => return make_response(StatusCode::METHOD_NOT_ALLOWED, "Not implemented"),
     };
 
-    let Some(response_res) = callback(path, request) else {
+    let Some(response_res) = callback(parts.uri.path(), request) else {
         return make_response(StatusCode::NOT_FOUND, "No valid route");
     };
 
@@ -144,10 +110,42 @@ fn generate_response(
 }
 
 pub fn handle(
-    mut stream: TcpStream,
+    stream: TcpStream,
     callback: impl Fn(&str, crate::Request) -> Option<crate::Response>,
 ) -> io::Result<()> {
-    let response = generate_response(&mut stream, callback);
-    let buf = stringify_response(response);
-    stream.write_all(&buf)
+    let mut reader = BufReader::new(stream.try_clone()?);
+    let mut writer = BufWriter::new(stream.try_clone()?);
+
+    loop {
+        let req = loop {
+            let buf = reader.fill_buf()?;
+            if buf.is_empty() {
+                stream.shutdown(Shutdown::Both)?;
+                return Ok(());
+            }
+
+            match parse_request(buf) {
+                Ok(None) => continue,
+                Ok(Some(req)) => {
+                    let amt = buf.len();
+                    reader.consume(amt);
+                    break Ok(req);
+                }
+                Err(resp) => {
+                    let amt = buf.len();
+                    reader.consume(amt);
+                    break Err(resp);
+                }
+            }
+        };
+
+        let resp = match req {
+            Ok(req) => generate_response(req, &callback),
+            Err(resp) => resp,
+        };
+
+        let buf = stringify_response(resp);
+        writer.write_all(&buf)?;
+        writer.flush()?;
+    }
 }
