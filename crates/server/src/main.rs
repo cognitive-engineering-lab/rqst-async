853b67ce4373bcfbc742dec2b96bac2a56df9a85
Will Crichton <wcrichto@cs.stanford.edu>
2024-07-29 16:11:51 -0700

    Meta

--- crates/server/src/main.rs
+++
@@ -1,9 +0,0 @@
-use miniserve::{Content, Request, Response};
-
-fn index(_req: Request) -> Response {
-    Ok(Content::Html(include_str!("../index.html").into()))
-}
-
-fn main() {
-    miniserve::Server::new().route("/", index).run()
-}
